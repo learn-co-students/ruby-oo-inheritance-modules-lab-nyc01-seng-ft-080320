abaae2245cd52876cf6e09fcec359a4f6b1c07fb
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-08 14:18:23 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,13 @@
 require 'pry'
 
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
 
 require_relative '../lib/artist.rb'
-require_relative '../lib/song.rb'
+require_relative '../lib/song.rb' 
+
+testArtist = Artist.new
+testArtist.name = 'kain'
+artist = Artist.find_by_name('kain')
+#binding.pry
