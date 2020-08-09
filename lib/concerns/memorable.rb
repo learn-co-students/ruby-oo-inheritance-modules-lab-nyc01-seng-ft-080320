687f7becd9a9fae581a912df6b4acc940149bcf1
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-09 11:48:22 -0400

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,29 @@
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+
+    def find_by_name(name)
+      self.all.find do |instance|
+        instance.name == name
+      end
+    end
+  end
+
+  module InstanceMethods
+
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
+
+    def initialize
+      self.class.all << self
+    end
+  end
+
+end
